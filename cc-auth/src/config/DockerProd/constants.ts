ac65fa979fde02c03a20a0c4cf43f61503764298
miyamoto <miyamoto.chichi@gmail.com>
2020-01-12 21:04:22 +0900

    modify port 3000 -> 8341/8342

--- cc-auth/src/config/DockerProd/constants.ts
+++ cc-auth/src/config/DockerProd/constants.ts
@@ -1,9 +1,9 @@
 // docker production
 // export const APP_HOST = 'localhost'
-export const APP_PORT = 3000
+export const APP_PORT = 8341
 
 export const API_HOST = 'json-server'
-export const API_PORT = 3000
+export const API_PORT = 8342
 export const REDIS_HOST = 'redis-server'
 export const REDIS_PORT = 6379
 
--- cc-auth/src/config/constants.ts
+++ cc-auth/src/config/constants.ts
@@ -1,6 +1,6 @@
 // node development
 // export const APP_HOST = 'localhost'
-export const APP_PORT = 3000
+export const APP_PORT = 8341
 
 export const API_HOST = 'localhost'
 export const API_PORT = 8342
